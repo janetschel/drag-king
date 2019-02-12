f38f774bcc67fc0e71b73cb483f8f6764fca1491
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2019-02-12 10:51:13 +0100

    get icon from filepath now works

--- gui/src/main/java/de/ergodirekt/drag/services/GetIconFromFilePath.java
+++ gui/src/main/java/de/ergodirekt/drag/services/GetIconFromFilePath.java
@@ -1,33 +1,12 @@
 package de.ergodirekt.drag.services;
 
 import sun.awt.shell.ShellFolder;
-
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
+import javax.swing.ImageIcon;
+import java.awt.Image;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class GetIconFromFilePath {
-
-    public static void main(String[] args) {
-        new GetIconFromFilePath();
-    }
-
-    private GetIconFromFilePath() {
-        JFrame frame = new JFrame("Test");
-        JLabel label = null;
-        try {
-            label = new JLabel(getIconFromFilePath("C:\\Users\\Unterwegs\\Documents\\GitHub\\AdventureGame\\res\\cloud.png", 100, 100));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        frame.setLayout(new BorderLayout());
-        frame.add(label, BorderLayout.CENTER);
-        frame.setSize(new Dimension(500, 500));
-        frame.setVisible(true);
-    }
-
+public abstract class GetIconFromFilePath {
     /**
      * Gibt das ImageIcon zur Datei - skaliert auf width und height - zurück
      * @param filePath Pfad zu der Datei
@@ -36,7 +15,7 @@ public class GetIconFromFilePath {
      * @return ImageIcon zum setzen auf ein Label oder Ähnliches
      * @throws FileNotFoundException, wenn die Datei nicht existiert
      */
-    public ImageIcon getIconFromFilePath(String filePath, int width, int height) throws FileNotFoundException {
+    public static ImageIcon getIconFromFilePath(String filePath, int width, int height) throws FileNotFoundException {
         Image image = ShellFolder.getShellFolder(new File(filePath)).getIcon(true);
         return new ImageIcon(width == 0 || height == 0 ? image : image.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH), null);
     }
@@ -47,7 +26,7 @@ public class GetIconFromFilePath {
      * @return ImageIcon zum setzen auf ein Label oder Ähnliches
      * @throws FileNotFoundException, wenn die Datei nicht existiert
      */
-    public ImageIcon getIconFromFilePath(String filePath) throws FileNotFoundException {
+    public static ImageIcon getIconFromFilePath(String filePath) throws FileNotFoundException {
         return getIconFromFilePath(filePath, 0, 0);
     }
 }
