26d3bd82125614711e8bf921a38da44e45374001
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2019-02-12 13:22:34 +0100

    get icon from filepath now works NICELY

--- gui/src/main/java/de/ergodirekt/drag/services/GetIconFromFilePath.java
+++ gui/src/main/java/de/ergodirekt/drag/services/GetIconFromFilePath.java
@@ -1,22 +1,37 @@
 package de.ergodirekt.drag.services;
 
-import sun.awt.shell.ShellFolder;
+import me.marnic.jiconextract.extractor.IconSize;
+import me.marnic.jiconextract.extractor.JIconExtractor;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public abstract class GetIconFromFilePath {
+public class GetIconFromFilePath {
     /**
      * Gibt das ImageIcon zur Datei - skaliert auf width und height - zurück
      * @param filePath Pfad zu der Datei
      * @param width Skalierung in x-Richtung
      * @param height Skalierung in y-Richtung
-     * @return ImageIcon zum setzen auf ein Label oder Ähnliches
-     * @throws FileNotFoundException, wenn die Datei nicht existiert
+     * @return ImageIcon zum setzen auf ein Label oder Ähnliches, Standard ist ein ImageIcon von einem Ordner
+     * @throws DateiExistiertNichtException, wenn die Standarddatei nicht existiert
      */
-    public static ImageIcon getIconFromFilePath(String filePath, int width, int height) throws FileNotFoundException {
-        Image image = ShellFolder.getShellFolder(new File(filePath)).getIcon(true);
+    public ImageIcon getIconFromFilePath(String filePath, int width, int height) throws DateiExistiertNichtException {
+        BufferedImage image;
+        String[] filePathParts = filePath.split("\\.");
+        String fileEnding = "." + (filePathParts.length > 1 ? filePathParts[filePathParts.length - 1] : "");
+
+        if(!fileEnding.equals(".")){
+            image = JIconExtractor.getJIconExtractor().extractIconFromFile(fileEnding, IconSize.JUMBO);
+        }else{
+            try {
+                image = ImageIO.read(new File("gui/src/main/java/de/ergodirekt/drag/services/ico/folder.png"));
+            } catch (IOException e) {
+                throw new DateiExistiertNichtException("ICO Datei für Ordner existiert nicht!");
+            }
+        }
+
         return new ImageIcon(width == 0 || height == 0 ? image : image.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH), null);
     }
 
@@ -24,9 +39,9 @@ public abstract class GetIconFromFilePath {
      * Gibt das ImageIcon zur Datei zurück
      * @param filePath Pfad zu der Datei
      * @return ImageIcon zum setzen auf ein Label oder Ähnliches
-     * @throws FileNotFoundException, wenn die Datei nicht existiert
+     * @throws DateiExistiertNichtException, wenn die Standarddatei nicht existiert
      */
-    public static ImageIcon getIconFromFilePath(String filePath) throws FileNotFoundException {
+    public ImageIcon getIconFromFilePath(String filePath) throws DateiExistiertNichtException {
         return getIconFromFilePath(filePath, 0, 0);
     }
 }
