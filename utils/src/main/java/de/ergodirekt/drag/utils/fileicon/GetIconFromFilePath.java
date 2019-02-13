1f133775d8534c0cd19cfffd647c5868a16da5e0
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2019-02-13 11:41:03 +0100

    updated

--- utils/src/main/java/de/ergodirekt/drag/utils/fileicon/GetIconFromFilePath.java
+++ utils/src/main/java/de/ergodirekt/drag/utils/fileicon/GetIconFromFilePath.java
@@ -7,9 +7,20 @@ import javax.swing.ImageIcon;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public abstract class GetIconFromFilePath {
     private static final int FILEPATH = 1;
+    private static File standardFile = null;
+    private static BufferedImage bufferedImage;
+
+    public static void main(String[] args) {
+        try {
+            getIconFromFilePath("");
+        } catch (DateiExistiertNichtException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Gibt das ImageIcon zur Datei - skaliert auf width und height - zurück
@@ -20,23 +31,13 @@ public abstract class GetIconFromFilePath {
      * @throws DateiExistiertNichtException, wenn die Standarddatei nicht existiert
      */
     public static ImageIcon getIconFromFilePath(String filePath, int width, int height) throws DateiExistiertNichtException {
-        String standardFilePath;
-        BufferedImage image;
         String[] filePathParts = filePath.split("\\.");
         String fileEnding = "." + (filePathParts.length > 1 ? filePathParts[filePathParts.length - 1] : "");
 
-        if(!fileEnding.equals(".")){
-            image = JIconExtractor.getJIconExtractor().extractIconFromFile(fileEnding, IconSize.JUMBO);
-        }else{
-            try {
-                standardFilePath = GetIconFromFilePath.class.getClassLoader().getResource("icons/folder.png").toString().split("^file:/")[FILEPATH];
-                image = ImageIO.read(new File(standardFilePath));
-            } catch (IOException e) {
-                throw new DateiExistiertNichtException("ICO Datei für Ordner existiert nicht!");
-            }
-        }
+        if(!fileEnding.equals(".")) bufferedImage = JIconExtractor.getJIconExtractor().extractIconFromFile(fileEnding, IconSize.JUMBO);
+        else bufferedImage = getStandardBufferedImage();
 
-        return new ImageIcon(width == 0 || height == 0 ? image : image.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH), null);
+        return new ImageIcon(width == 0 || height == 0 ? bufferedImage : bufferedImage.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH), null);
     }
 
     /**
@@ -47,5 +48,24 @@ public abstract class GetIconFromFilePath {
      */
     public static ImageIcon getIconFromFilePath(String filePath) throws DateiExistiertNichtException {
         return getIconFromFilePath(filePath, 0, 0);
+    }
+
+
+    // Returnt ein Standard-BufferedImage, wenn kein anderes Icon existiert
+    private static BufferedImage getStandardBufferedImage() throws DateiExistiertNichtException {
+        if (bufferedImage == null){
+            standardFile = getStandardFile();
+            try {
+                bufferedImage = ImageIO.read(standardFile);
+            } catch (IOException | IllegalArgumentException e) {
+                throw new DateiExistiertNichtException("ICO für Standarddatei existiert nicht!");
+            }
+        }
+        return bufferedImage;
+    }
+
+    private static File getStandardFile() {
+        URL url = GetIconFromFilePath.class.getClassLoader().getResource("icons/folder.png");
+        return standardFile == null ? (url == null ? null : new File(url.toString().split("^file:/")[FILEPATH]) ) : standardFile;
     }
 }
