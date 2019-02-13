9b78687c69348a5a03700b48258a7c3964446e4c
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2019-02-13 13:14:48 +0100

    updated updated JUnit test for GetIconFromFilePath.java

--- utils/src/test/java/de/ergodirekt/drag/utils/fileicon/GetIconFromFilePathTest.java
+++ utils/src/test/java/de/ergodirekt/drag/utils/fileicon/GetIconFromFilePathTest.java
@@ -10,49 +10,50 @@ public class GetIconFromFilePathTest {
     private String validPath2 = ".";
     private String notValidPath = null;
 
-    private ImageIcon iconFromPath1 = null;
-    private ImageIcon iconFromPath2 = null;
-    private ImageIcon iconFromPath3 = null;
-
     @Test
     public void getIconFromFilePath() {
+        ImageIcon iconFromPath1;
+        ImageIcon iconFromPath2;
+        ImageIcon iconFromPath3;
+
         try {
             iconFromPath1 = GetIconFromFilePath.getIconFromFilePath(validPath, 100, 100);
-            iconFromPath2 = GetIconFromFilePath.getIconFromFilePath(validPath2, 100, 100);
-            // Hier ist alles richtig
-        } catch (DateiExistiertNichtException e) { Assert.fail("Icon sollte bekommen werden, Pfad ist richtig!"); }
+            Assert.assertNotNull(iconFromPath1);
 
-        // Diese beiden Statements dürfen nicht null sein, da sie von einen Pfad kommen, der richtig ist
-        Assert.assertNotNull(iconFromPath1);
-        Assert.assertNotNull(iconFromPath2);
+            iconFromPath2 = GetIconFromFilePath.getIconFromFilePath(validPath2, 100, 100);
+            Assert.assertNotNull(iconFromPath2);
+
+        } catch (DateiExistiertNichtException e) { Assert.fail("Icon sollte bekommen werden, Pfad ist richtig!"); }
 
         try {
             iconFromPath3 = GetIconFromFilePath.getIconFromFilePath(notValidPath, 100, 100);
+            Assert.assertNull(iconFromPath3);
+
             Assert.fail("Icon sollte nicht bekommen werden, Pfad ist falsch!");
         } catch (DateiExistiertNichtException | NullPointerException e) { /* Hier ist alles richig */ }
-
-        // Soll null sein, da der Pfad nicht richtig war
-        Assert.assertNull(iconFromPath3);
     }
 
     @Test
     public void getIconFromFilePathWithNoArguments() {
+        ImageIcon iconFromPath1;
+        ImageIcon iconFromPath2;
+        ImageIcon iconFromPath3;
+
         try {
             iconFromPath1 = GetIconFromFilePath.getIconFromFilePath(validPath);
+            Assert.assertNotNull(iconFromPath1);
+
             iconFromPath2 = GetIconFromFilePath.getIconFromFilePath(validPath2);
+            Assert.assertNotNull(iconFromPath2);
+
             // Hier ist alles richtig
         } catch (DateiExistiertNichtException e) { Assert.fail("Icon sollte bekommen werden, Pfad ist richtig!"); }
 
-        // Diese beiden Statements dürfen nicht null sein, da sie von einen Pfad kommen, der richtig ist
-        Assert.assertNotNull(iconFromPath1);
-        Assert.assertNotNull(iconFromPath2);
-
         try {
             iconFromPath3 = GetIconFromFilePath.getIconFromFilePath(notValidPath);
+            Assert.assertNull(iconFromPath3);
+
             Assert.fail("Icon sollte nicht bekommen werden, Pfad ist falsch!");
         } catch (DateiExistiertNichtException | NullPointerException e) { /* Hier ist alles richig */ }
-
-        // Soll null sein, da der Pfad nicht richtig war
-        Assert.assertNull(iconFromPath3);
     }
 }
